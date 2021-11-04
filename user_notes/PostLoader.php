2ae00a1fa3c095b0f515191abd4448439a1ae04e
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2021-11-04 10:13:12 +0100

    json decode
    json decode is working; I could print the first message on the browser.

--- user_notes/PostLoader.php
+++ user_notes/PostLoader.php
@@ -35,23 +35,34 @@ class PostLoader
         $date = $this->Date;
         $date = date('l \t\h\e jS');
         $data = json_decode(file_get_contents($this->feedback));
-        $data[] = array("author" => $this->Author, "lname" => $_POST["lname"], "usertext" => $_POST["usertext"], "date" => $date);
+        $data[] = array("author" => $_POST["fname"], "lname" => $_POST["lname"], "usertext" => $_POST["usertext"], "date" => $date);
         $json = json_encode($data);
         file_put_contents($this->feedback, $json);
-        var_dump(json_encode($data));
+        // var_dump(json_encode($data));
+        $this->getDataFromJson();
+        
     }
 
 
 
-    public function storeDataInJson()
+    public function getDataFromJson()
     {
 
-
-
-        // var_dump(json_encode($this->userarray));
-        // // $file = json_encode($this->userarray);
-        // // file_put_contents($this->feedback,$file);
-
-
+        $getData = file_get_contents($this->feedback);
+        $json = json_decode($getData);
+        var_dump($json);
+        // var_dump($getData->author);
+        // $this->pasteTheDataInBrowser();
+       echo $json[0]->author. "<br>";
+       echo $json[0]->lname. "<br>";
+       echo $json[0]->usertext. "<br>";
+       echo $json[0]->date. "<br>";
     }
+
+    // public function pasteTheDataInBrowser()
+    // {
+    //     foreach ($this->getData as $getData_num => $data) {
+    //         echo "Line #<b>{$getData_num}</b> : " . htmlspecialchars($data) . "<br />\n";
+    //     }
+    // }
 }
